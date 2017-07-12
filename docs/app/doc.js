b91abe13c3d7c8e365a29de21b84f29181a0c68d
tower1229 <tower1229@qq.com>
2017-07-12 08:13:25 +0800

    test

--- docs/app/doc.js
+++ docs/app/doc.js
@@ -10,8 +10,8 @@ define(function(require) {
 		url: 'http://host.refined-x.com/get.php',
 		data: {
 			url: 'http%3A%2F%2Fwww.apicloud.com%2FgetAllUnpack%3FappId%3DA6997660453388%26startNum%3D0%26num%3D5%26type%3D1',
-			Referer: 'http://www.apicloud.com/package',
-			Host: 'www.apicloud.com'
+			refer: 'http://www.apicloud.com/package',
+			host: 'www.apicloud.com'
 		},
 		dataType: 'json',
 		success: function(res){
