9d209bd10ee602c14618610a1fc06c2b5858a6f2
tower1229 <tower1229@foxmail.com>
2018-09-15 09:28:44 +0800

    update

--- view/demo/ota/script.js
+++ view/demo/ota/script.js
@@ -9,7 +9,7 @@ define(function(require) {
 	var zip = api.require('zip');
 
 	//下载配置
-	var otaPath = "fs://Download/";
+	var otaPath = "fs://plugins/";
 
 	/*
 	* 插件列表数据
@@ -91,7 +91,6 @@ define(function(require) {
 				url: pagePath
 			});
 	    } else {
-	    	app.toast('正在下载');
 	        downloadPlugin(item);
 	    }
 	};
@@ -103,6 +102,7 @@ define(function(require) {
 		if(!item.remote || !item.name){
 			return app.toast("该插件暂不可用");
 		}
+		app.toast('正在安装');
 		app.loading.show();
 		download(item.remote,{
 			path: otaPath,
@@ -125,7 +125,7 @@ define(function(require) {
 				        myRender.data({
 							data: localPluginsData
 						});
-						app.toast('插件下载完成');
+						app.toast('插件安装完成');
 				    } else {
 				        alert(JSON.stringify(err));
 				    }
